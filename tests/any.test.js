e2322a7423199efe20e13996cd02fa83fea2bc5b
emilwerner <emilwerner123@hotmail.com>
2015-02-02 22:36:06 +0100

    Test

---
+++ tests/any.test.js
@@ -0,0 +1,40 @@
+
+describe("Any is implemented correctly", function() {
+	it("Any exists", function() {
+		expect([].any).toBeDefined();
+	});
+
+	it("First on empty array returns false", function() {
+		expect([].any()).toEqual(false);
+	});
+
+	it("First on non-empty array returns false", function() {
+		expect([1,2,3].any()).toEqual(true);
+	});
+
+	it("First with func on empty array returns false", function() {
+		expect([].any(function(item){
+			return item === 1;
+		})).toEqual(false);
+	});
+
+
+	it("First with func on non-empty array returns false", function() {
+		expect([1,2,3,4,5].any(function(item){
+			return item === 1;
+		})).toEqual(true);
+	});
+
+	it("Works with complex objects", function() {
+		var personArray = [{name:"Emil", age: 24},{name:"Anders", age: 19},{name:"Per", age: 21},{name:"Marko", age: 17}];
+
+		expect(personArray.any(function(item){
+			return item.name === "Emil";
+		})).toEqual(true);
+
+		expect(personArray.any(function(item){
+			return item.name === "Kalle";
+		})).toEqual(false);
+	});
+
+});
